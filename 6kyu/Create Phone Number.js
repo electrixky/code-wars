1aa16627a8e1917c7bb23ba43e1022468bc6ee89
electrixky <tanyatkachuk2401@gmail.com>
2024-01-09 14:02:28 +0200

    create-phone-numbers 6kyu kata added solution 2

--- 6kyu/Create Phone Number.js
+++ 6kyu/Create Phone Number.js
@@ -1,9 +1,9 @@
 // Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 //
-//     Example
+// Example
 // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
 
-// initial solution
+// ########## initial solution ################
 const isInTheRange = (currentValue) => currentValue >= 0 && currentValue < 10;
 
 function createPhoneNumber(numbers) {
@@ -16,6 +16,18 @@ function createPhoneNumber(numbers) {
     return "Invalid input";
 }
 
-createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
 
-// using replace()
+
+// ########### using replace() ##################
+function createPhoneNumber(numbers) {
+    if(numbers.length === 10 && numbers.every(num => num >= 0 && num <10)) {
+        let format = "(xxx) xxx-xxxx"
+
+        let index = 0
+        return format.replace(/x/g, () => numbers[index++])
+    }
+    return "Invalid value"
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
