28399b116ad176e58e7effccef02446fccadc225
electrixky <tanyatkachuk2401@gmail.com>
2024-01-10 11:55:20 +0200

    solution 2 added

--- 6kyu/Stop gninnipS My sdroW.js
+++ 6kyu/Stop gninnipS My sdroW.js
@@ -1,20 +1,34 @@
 // Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
 //######### initial solution ###########
-function spinWords(string){
+// function spinWords(string){
+//
+//     const splitStr = string.split(" ")
+//     let newArr = []
+//
+//     splitStr.map(word => {
+//         if (word.length < 5) {
+//             newArr.push(word)
+//         } else {
+//             newArr.push(word.split("").reverse().join(""))
+//         }
+//     })
+//
+//     return newArr.join(" ")
+// }
+//
+// console.log(spinWords("Hey fellow warriors"))
 
-    const splitStr = string.split(" ")
-    let newArr = []
+//######### using ternary operator ###########
 
-    splitStr.map(word => {
-        if (word.length < 5) {
-            newArr.push(word)
-        } else {
-            newArr.push(word.split("").reverse().join(""))
-        }
-    })
-
-    return newArr.join(" ")
+function spinWords(words) {
+    return words.split(" ").map(word => {
+        return (word.length > 4) ? word.split("").reverse().join("") : word
+    }).join(" ")
 }
 
 console.log(spinWords("Hey fellow warriors"))
+
+
+//######### using regEx ###########
+
