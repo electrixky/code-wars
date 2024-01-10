5bba70220348543bdf62558e0f9926457b061cc8
electrixky <tanyatkachuk2401@gmail.com>
2024-01-10 09:57:41 +0200

    create-phone-numbers 6kyu kata added solution 4

--- 6kyu/Create Phone Number.js
+++ 6kyu/Create Phone Number.js
@@ -33,12 +33,21 @@
 // console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
 
 
-// ########## using substring ###################
+// ########## using substring() ###################
+
+// function createPhoneNumber(numbers) {
+//     let numberString = numbers.join("")
+//
+//     return "(" + numberString.substring(0, 3) + ") " + numberString.substring(3, 6) + "-" + numberString.substring(6)
+// }
+//
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+
+
+// ########### using regEx ##################
 
 function createPhoneNumber(numbers) {
-    let numberString = numbers.join("")
-
-    return "(" + numberString.substring(0, 3) + ") " + numberString.substring(3, 6) + "-" + numberString.substring(6)
+    return numbers.join('').replace(/(...)(...)(.*)/,'($1) $2-$3')
 }
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
