0dccefb9d8fc99a129cf34c0a78505aa889d81f5
electrixky <tanyatkachuk2401@gmail.com>
2024-01-11 20:14:31 +0200

    counting-duplicates 6kyu kata added solution 1

---
+++ 6kyu/Counting Duplicates.js
@@ -0,0 +1,26 @@
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// ######## initial solution ########
+function duplicateCount(text){
+    let charCounterObj = {}
+    let counter = 0
+    const textArr = text.toLowerCase().split("")
+
+    for (let i = 0; i < textArr.length; i++) {
+        if (textArr[i] in charCounterObj) {
+            charCounterObj[textArr[i]]++
+        } else {
+            charCounterObj[textArr[i]] = 1
+        }
+    }
+
+    for (let prop in charCounterObj) {
+        if(charCounterObj.hasOwnProperty(prop) && charCounterObj[prop]>=2) {
+            counter++
+        }
+    }
+    return counter
+}
+
+console.log(duplicateCount("Indivisibilities"))
