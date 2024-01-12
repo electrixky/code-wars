6e74eb5089ef23f7ebe7675fc6d2f2ee359bf23f
electrixky <tanyatkachuk2401@gmail.com>
2024-01-12 11:08:54 +0200

    counting-duplicates 7kyu kata added solution 2

--- 7kyu/You re a square.js
+++ 7kyu/You re a square.js
@@ -1,8 +1,16 @@
-//
+// Given an integral number, determine if it's a square number:
 
-// ######## initial solution ########
+######## initial solution ########
 var isSquare = function(n){
     return Number.isInteger(Math.sqrt(n))
 }
 
 console.log(isSquare(4))
+
+// ######## using modulus #########
+
+var isSquare = function(n){
+    return Math.sqrt(n)%1 === 0
+}
+
+console.log(isSquare(25))
