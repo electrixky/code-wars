aff195b6aae05f36040cb158814d462302eb437d
electrixky <tanyatkachuk2401@gmail.com>
2024-01-15 23:28:25 +0200

    split-in-parts  7kyu kata added solution 2

--- 7kyu/Split In Parts.js
+++ 7kyu/Split In Parts.js
@@ -1,13 +1,26 @@
 //The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
 
 //###### initial solution ###########
-var splitInParts = function(s, partLength){
+let splitInParts = function(s, partLength){
     const resultArr = []
     for (let i=0; i<s.length; i+=partLength) {
         let substring = s.slice(i,i+partLength)
         resultArr.push(substring)
     }
     return resultArr.join(" ")
+}
+
+console.log(splitInParts("supercalifragilisticexpialidocious", 3))
+
+//###### with splice ###########
+
+let splitInParts = function(s, partLength) {
+    let parts = []
+    let array = s.split("")
+    while(array.length) {
+        parts.push(array.splice(0,partLength).join(""))
+    }
+    return parts.join(" ")
 }
 
 console.log(splitInParts("supercalifragilisticexpialidocious", 3))
