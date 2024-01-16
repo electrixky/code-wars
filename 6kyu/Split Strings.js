dc506b7231a55d529de67c125217b31fa0f625ef
electrixky <tanyatkachuk2401@gmail.com>
2024-01-16 14:10:00 +0200

    split-strings  6kyu kata added solution 1

---
+++ 6kyu/Split Strings.js
@@ -0,0 +1,20 @@
+//Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+// ###### initial solution ##########
+function solution(str) {
+    let newArr = []
+
+    for (let i = 0; i < str.length; i += 2) {
+        newArr.push(str.slice(i, i + 2))
+    }
+    if (newArr.length > 0 && newArr[newArr.length - 1].length === 1) {
+        newArr[newArr.length - 1] += '_'
+    }
+    return newArr
+
+}
+
+console.log(solution("abcdefg"))
+console.log(solution(""))
+
+
+
