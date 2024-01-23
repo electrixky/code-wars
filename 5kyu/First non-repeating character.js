bd9233cb12e7c637877a68ed9b607e663066a545
electrixky <tanyatkachuk2401@gmail.com>
2024-01-23 18:51:29 +0200

    first-non-repeating-character  5kyu kata added solution 2

--- 5kyu/First non-repeating character.js
+++ 5kyu/First non-repeating character.js
@@ -1,20 +1,32 @@
 //Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
 //###### initial solution #######
 
+// function firstNonRepeatingLetter(str) {
+//     const letters = {}
+//
+//     for (const char of str) {
+//         const lowerChar = char.toLowerCase()
+//
+//         letters.hasOwnProperty(lowerChar) ? letters[lowerChar]++ : letters[lowerChar] = {count: 1, originalCase: char}
+//     }
+//     for (const char in letters) {
+//         if(letters[char].count === 1) {
+//             return letters[char].originalCase
+//         }
+//     }
+//
+//     return ''
+// }
+//
+// console.log(firstNonRepeatingLetter('sTreSS'))
+
+//###### using RegEx #######
 function firstNonRepeatingLetter(str) {
-    const letters = {}
-
-    for (const char of str) {
-        const lowerChar = char.toLowerCase()
-
-        letters.hasOwnProperty(lowerChar) ? letters[lowerChar]++ : letters[lowerChar] = {count: 1, originalCase: char}
-    }
-    for (const char in letters) {
-        if(letters[char].count === 1) {
-            return letters[char].originalCase
+    for (const char in str) {
+        if(str.match(new RegExp(str[char],"gi")).length === 1) {
+            return str[char]
         }
     }
-
     return ''
 }
 
