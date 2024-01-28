3c32a9cdabedc5b68136fff355b90d563e03c010
electrixky <tanyatkachuk2401@gmail.com>
2024-01-28 19:26:33 +0200

    factorial 7kyu kata added solution 1

---
+++ 7kyu/Factorial.js
@@ -0,0 +1,18 @@
+// Write a function to calculate factorial for a given input.  If input is below 0 or above 12 throw an exception of type "RangeError"
+
+//#### initial solution #####
+
+function factorial(n) {
+    if (n >= 0 && n <= 12) {
+        let factorial = 1
+        for (let i = n; i > 0; i--) {
+            factorial *= i
+        }
+
+        return factorial
+    } else {
+        throw new RangeError("Out of range");
+    }
+}
+
+console.log(factorial(5))
