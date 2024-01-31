30a19036d2eccbcbf103a0e6c662cea86fe41f52
electrixky <tanyatkachuk2401@gmail.com>
2024-01-31 10:20:26 +0200

    Minimize Sum Of Array 7kyu kata added solution 3

--- 7kyu/Playing With Lists/1 Minimize Sum Of Array.js
+++ 7kyu/Playing With Lists/1 Minimize Sum Of Array.js
@@ -1,15 +1,15 @@
 //Find the minimum sum which is obtained from summing each Two integers product . For example, minSum([12,6,10,26,3,24]) returns 342 because  26*3 + 24*6 + 12*10 = 342
 //###### initial solution #######
-// function minSum(arr) {
-//     const sortedArr = arr.sort((a,b)=> a-b)
-//     let sum = 0
-//     for (let i = 0; i < sortedArr.length/2; i++) {
-//         sum += sortedArr[i] * sortedArr[sortedArr.length-i-1]
-//     }
-//     return sum
-// }
-//
-// console.log(minSum([9,2,8,7,5,4,0,6]))
+function minSum(arr) {
+    const sortedArr = arr.sort((a,b)=> a-b)
+    let sum = 0
+    for (let i = 0; i < sortedArr.length/2; i++) {
+        sum += sortedArr[i] * sortedArr[sortedArr.length-i-1]
+    }
+    return sum
+}
+
+console.log(minSum([9,2,8,7,5,4,0,6]))
 
 //###### using reduce #######
 function minSum(arr) {
@@ -17,3 +17,18 @@ function minSum(arr) {
 }
 
 console.log(minSum([12,6,10,26,3,24]))
+
+//##### pop() and shift() #######
+
+function minSum(arr) {
+    arr.sort((a, b) => a - b)
+    let sum = 0
+
+    while (arr.length) {
+        sum += arr.pop() * arr.shift()
+    }
+
+    return sum
+}
+
+console.log(minSum([12, 6, 10, 26, 3, 24]))
