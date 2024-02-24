1026130002457cda0c5e7c5f53ca59013beab92d
electrixky <tanyatkachuk2401@gmail.com>
2024-02-24 10:33:47 +0200

    Days in the year 8kyu kata added solution 2

--- 8kyu/Days in the year.js
+++ 8kyu/Days in the year.js
@@ -1,13 +1,20 @@
 //A variation of determining leap years, assuming only integers are used and years can be negative and positive.
 //#### initial solution ######
 
+// function yearDays(year) {
+//     const LEAP_YEAR = 366
+//     const NON_LEAP_YEAR = 365
+//     if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//             return `${year} has ${LEAP_YEAR} days`
+//     }
+//     return `${year} has ${NON_LEAP_YEAR} days`
+// }
+//
+// console.log(yearDays(300))
+
+//#### solution 2 ######
 function yearDays(year) {
-    const LEAP_YEAR = 366
-    const NON_LEAP_YEAR = 365
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-            return `${year} has ${LEAP_YEAR} days`
-    }
-    return `${year} has ${NON_LEAP_YEAR} days`
+    return year + " has " + (!(year % 100) && year % 400 || year % 4 ? "365" : "366") + " days"
 }
 
-console.log(yearDays(300))
+console.log(yearDays(-400))
